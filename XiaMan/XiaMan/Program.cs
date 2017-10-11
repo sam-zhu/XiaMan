53a588f02f480e0e7047c2524a52f431bc9f400e
朱聪 <zhucon@shanghai3h.com>
2017-10-11 17:20:10 +0800

    添加：基础project, 快速排序算法

---
+++ XiaMan/XiaMan/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XiaMan.Algorithm;
+
+namespace XiaMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = { 10, 5, 3, 1, 7, 2, 8 };
+            Console.WriteLine("排序之前：");
+            foreach (var element in array)
+            {
+                Console.Write(element + " ");
+            }
+
+            Sort.quickSort(array, 0, array.Length - 1);
+
+            Console.WriteLine("\n排序之后：");
+            foreach (var element in array)
+            {
+                Console.Write(element + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
